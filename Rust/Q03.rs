b3ec2fa2aef1e751db20fbe59999e766484d9f6f
pankaj pawar <79073431+pankajpawar001@users.noreply.github.com>
2024-07-04 16:56:25 +0530

    Delete Rust directory

--- Rust/Q03.rs
+++
@@ -1,9 +0,0 @@
-fn shortest_word_in_string(input: &str) -> &str {
-    input.split_whitespace().min_by_key(|word| word.len()).unwrap_or("")
-}
-
-fn main() {
-    let input = "This is a test string with words of varying lengths";
-    let shortest_word = shortest_word_in_string(input);
-    println!("The shortest word in the string is: {}", shortest_word);
-}
